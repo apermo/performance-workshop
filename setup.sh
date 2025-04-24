8a20bb24fdbedba8a3f713bc3548f44f189c16cf
Christoph Daum <c.daum@me.com>
2025-04-24 13:25:47 +0200

    docs: Added docs to setup.sh

--- setup.sh
+++ setup.sh
@@ -6,8 +6,10 @@ cp .env.example .env
 
 curl -o performance.sql.gz https://christoph-daum.com/wp-content/uploads/workshop/db_2025-04-24.sql.gz
 gunzip performance.sql.gz
+echo "Importing Database, this will take a few minutes... please be patient"
 ddev import-db < performance.sql
 rm performance.sql
 ddev wp fill-content
+echo "Importing done, starting the site..."
 
 ddev launch /wp/wp-login.php
